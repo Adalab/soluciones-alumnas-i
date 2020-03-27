5e6aadcd05ca531c63ec2a5e7e93377eb41712b9
AnaValca <avc92j@gmail.com>
2020-03-27 10:41:46 +0100

    add ex3 of lesson1-module 3

---
+++ modulo-3/lesson-1/Exercise-3-Ana-Valdivia/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Prueba los ejemplos anteriores exportando datos desde un fichero e importándolos desde otros.
+// Asegúrate de entender bien cómo funcionan las rutas para importar/exportar adecuadamente.
+
+import { Square, mySquare } from './square.js';
+
+const newSquare = new Square();
+
+
+console.log(newSquare.perimeter(9));
+console.log(newSquare.area(9));
+
---
+++ modulo-3/lesson-1/Exercise-3-Ana-Valdivia/square.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Vamos a crear un programita que haga cálculos geométricos sencillos.
+// En el programa definiremos la clase Square, que tendrá al menos:
+// Un método para calcular el perímetro (perimeter()) del cuadrado (multiplica la longitud del lado por el número de lados)
+// Un método para calcular el área (area()) del cuadrado (elevar al cuadrado el lado)
+// Los métodos recibirán la longitud del lado (side) como parámetro.
+// Calcularemos y mostraremos el perímetro y el área de un cuadrado de 9 de lado.
+
+let elementside = 9;
+
+class Square {
+    perimeter(side){
+        return side * 4;
+    }
+    area(side){
+        return side * side;
+    }
+}
+
+const mySquare = new Square();
+
+// console.log(`El cuadrado tiene un perimetro de ${mySquare.perimeter(elementside)}cm`);
+// console.log(`El cuadrado tiene un area de ${mySquare.area(elementside)}cm`);
+
+export { Square, mySquare };
